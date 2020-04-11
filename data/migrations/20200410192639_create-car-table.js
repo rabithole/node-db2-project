a52da3ba21248c357d11495d20251bfc70cbd9fb
Robert Kulp <pedalinon@gmail.com>
2020-04-10 19:42:58 -0700

    migrations completed. Working on createTable function in create car table file

--- data/migrations/20200410192639_create-car-table.js
+++ data/migrations/20200410192639_create-car-table.js
@@ -6,10 +6,15 @@ exports.up = function(knex) {
   	tbl.text('carId', 128) // 128 is a character limit
   		.unique()	// unique and notNullable are contraints on the data. 
   		.notNullable();
-  	tbl.text('website');
+  	tbl.text('make');
+  	tbl.text('model');
+  	tbl.text('VIN');
+  	tbl.text('mileage');
+  	tbl.text('');
   })
 };
 
 exports.down = function(knex) {
   // This undo's the schema. Used for making changes to the schema.
+  return knex.schema.dropTableIfExists('cars');
 };
